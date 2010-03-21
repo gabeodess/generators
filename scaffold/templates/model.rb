0d93f6766555ba3ee582497ab51bb4df67a9b099
Gabe Odess <gabeodess@Macintosh-253.local>
2010-03-21 13:05:33 -0400

    working on currencies

--- scaffold/templates/model.rb
+++ scaffold/templates/model.rb
@@ -1,5 +1,8 @@
 class <%= class_name %> < ActiveRecord::Base
   
+  # ================
+  # = Associations =
+  # ================
   <%- associations.each do |association| -%>
     <%= association.keys.first %> <%= association.values.first %>
   <%- end -%>
@@ -7,5 +10,17 @@ class <%= class_name %> < ActiveRecord::Base
   <%- paperclips.each do |paperclip| -%>
     has_attached_file :<%= paperclip %>
   <%- end -%>
+  
+  <% if !currencies.blank? %>
+    <% for currency in currencies %>
+      validates_format_of :<%= currency %>, :unless => match_currency_format
+    <% end %>
+    
+    def match_currency_format
+      
+    end
+  <% else %>
+    
+  <% end %>
   
 end
