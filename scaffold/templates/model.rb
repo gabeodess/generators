68c9a95accd256254a1f8eb017d04dec3d6e6702
Gabe Odess <gabeodess@gmail.com>
2010-03-23 08:57:07 -0400

    changes

--- scaffold/templates/model.rb
+++ scaffold/templates/model.rb
@@ -3,24 +3,37 @@ class <%= class_name %> < ActiveRecord::Base
   # ================
   # = Associations =
   # ================
-  <%- associations.each do |association| -%>
-    <%= association.keys.first %> <%= association.values.first %>
-  <%- end -%>
+<%- associations.each do |association| -%>
+  <%= association.keys.first %> <%= association.values.first %>
+<%- end -%>
   
-  <%- paperclips.each do |paperclip| -%>
-    has_attached_file :<%= paperclip %>
-  <%- end -%>
+<%- paperclips.each do |paperclip| -%>
+  has_attached_file :<%= paperclip %>
+<%- end -%>
   
-  <% if !currencies.blank? %>
-    <% for currency in currencies %>
-      validates_format_of :<%= currency %>, :unless => match_currency_format
-    <% end %>
-    
-    def match_currency_format
-      
-    end
-  <% else %>
-    
+<% if !currencies.blank? %>
+  # ==============
+  # = Attributes =
+  # ==============
+  attr_accessor <%= currencies.map{|item| ":your_#{item}"}.join(', ') %>
+  
+  # ===============
+  # = Validations =
+  # ===============
+  <% for currency in currencies %>
+  validates_format_of :your_<%= currency %>, :with => Validator.currency_regex, :allow_blank => true
   <% end %>
+  
+  # =========
+  # = Hooks =
+  # =========
+  before_save <%= currencies.map{|item| ":set_#{item}"}.join(', ') %>
+  
+  <%- currencies.each do |currency| %>
+  def set_<%= currency %>
+    self.<%= currency %> = your_<%= currency %>.gsub(/\$|,/, '') if your_<%= currency %>
+  end
+  <%- end -%>
+<% end %>
   
 end
