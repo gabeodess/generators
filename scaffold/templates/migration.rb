8223f82f0581c71a98cc5932d572933d39b8fdb2
Gabe Odess <gabeodess@Macintosh-253.local>
2010-08-04 08:45:00 -0400

    added indexing for belongs to associations

--- scaffold/templates/migration.rb
+++ scaffold/templates/migration.rb
@@ -8,9 +8,15 @@ class Create<%= plural_class_name %> < ActiveRecord::Migration
       t.timestamps
     <%- end -%>
     end
+  <%- for association in associations.select{|item| item.keys.include?("belongs_to")} -%>
+    add_index :<%= plural_name %>, :<%= association['belongs_to'] %>_id
+  <%- end -%>
   end
   
   def self.down
+  <%- for association in associations.select{|item| item.keys.include?("belongs_to")} -%>
+    remove_index :<%= plural_name %>, :<%= association['belongs_to'] %>_id
+  <%- end -%>
     drop_table :<%= plural_name %>
   end
 end
